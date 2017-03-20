23bb93b12e178b4ada8faed63210ee781512a85d
vervin <vervin@163.com>
2017-03-20 10:11:36 +0800

    add store
    add store

--- src/main.js
+++ src/main.js
@@ -1,15 +1,60 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import store from './store'
 import App from './App'
-import router from './router'
+import Home from './components/Home'
+import TimeEntries from './components/TimeEntries.vue'
+import LogTime from './components/LogTime.vue'
+import NotFound from './components/404'
+import VueResource from 'vue-resource'
+import 'bootstrap/dist/css/bootstrap.css'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const routes = [{
+  path : '/',
+  component : Home
+},{
+  path : '/home',
+  component : Home
+},{
+  path : '/time-entries',
+  component : TimeEntries,
+  children : [{
+    path : 'log-time',
+    component : LogTime,
+  }]
+},{
+  path : '*',
+  component : NotFound
+}];
+
+const router = new VueRouter({
+  routes
+});
 
 /* eslint-disable no-new */
+// 这灵活得亮瞎了
+/*new Vue({
+  el: '#app',
+  template: '<App/>',
+  router,
+  components: { App }
+});
+
+ new Vue(Vue.util.extend({
+   router
+ }, App)).$mount('#app');
+
 new Vue({
+  el:'#app',
+  router,
+  render:h => h(App)
+});*/
+var app = new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-})
+  store,
+  ...App,
+});
